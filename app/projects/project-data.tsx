2ba19a86cc9c8e927e6bd122d5c5da69f971d2c8
MovieGoers <alscjf4104@gmail.com>
2025-07-05 00:47:22 +0900

    add : 프로젝트 수정.

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,15 +7,31 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Mithril AI",
-    year: 2024,
-    description: "Open science AI resarch lab",
-    url: "https://github.com/mithrilai",
+    title: "다시, 봄 (Re:Spring)",
+    year: 2025,
+    description:
+      "퇴직자들을 위한 웹/앱 공익 서비스",
+    url: "https://reinvented-perfume-2f5.notion.site/Re-Spring-15a32d73ef708171aba2f1fd0fd1a12c",
   },
   {
-    title: "OpenDeepLearning",
-    year: 2023,
-    description: "Open source AI education resources",
-    url: "https://opendeeplearning.xyz/",
+    title: "사각 (Square)",
+    year: 2025,
+    description:
+      "사용자 통계 및 투표 기반 모바일 토론 플랫폼",
+    url: "https://reinvented-perfume-2f5.notion.site/Square-1da32d73ef70809990d1e44edf0d6ccb",
+  },
+  {
+    title: "집피티 (ZipPT)",
+    year: 2024,
+    description:
+      "ChatGPT 기반 부동산 챗봇 웹 서비스",
+    url: "https://reinvented-perfume-2f5.notion.site/ZipPT-15a32d73ef7081e5b8e7ce99f9673690",
+  },
+  {
+    title: "시카 (CICA)",
+    year: 2025,
+    description:
+      "콜센터 상담사를 위한 AI 상담 보조 프로그램",
+    url: "https://reinvented-perfume-2f5.notion.site/CICA-1ff32d73ef7080dd8442e00aaec85450",
   },
 ];
