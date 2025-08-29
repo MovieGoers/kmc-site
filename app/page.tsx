e1e0c944abf03fe24197adc7249eecfb395a8794
MovieGoers <alscjf4104@gmail.com>
2025-08-30 02:23:40 +0900

    add: 인턴 내용 추가.

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,11 @@ export default function Page() {
 
         <h2 className="mt-8 text-xl font-medium">경력 및 교육</h2>
         <ul>
-          <li><strong>엔테크서비스(NTS) UI 개발 인턴 (2025.07 ~ 2025.09)</strong></li>
+          <li>
+            <strong>엔테크서비스(NTS) UI 개발 인턴 (2025.07 ~ 2025.09)</strong>
+            <div className="text-sm">(Gulp Task Runner, SCSS, 웹 표준, 접근성 HTML5, CSS3 UI 개발)</div>
+          </li>
+          
           <li>
             <strong>삼성 청년 SW 아카데미 12기 자바 전공반 1200시간 (2024.07 ~ 2025.06)</strong>
             <div className="text-sm">(Java, 알고리즘, SpringBoot, MySQL, Vue.js, React.js, React Native 학습 및 팀 프로젝트 진행)</div>
